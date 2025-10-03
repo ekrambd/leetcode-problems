b1ca9a6dfbb03127e24d7a6e0dca169829c8eaac
ekrambd <ekramhossainekram28@gmail.com>
2025-10-03 23:03:39 +0600

    add two probles

---
+++ array-13-problems/leetcode_344.js
@@ -0,0 +1,16 @@
+//344. Reverse String
+
+var reverseString = function(s) {
+    let left = 0;
+    let right = s.length - 1;
+    while(left < right){
+        let temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        left++;
+        right--;
+    }
+    return s;
+};
+
+console.log(reverseString(["h","e","l","l","o"]));
